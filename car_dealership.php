6873635e4ab5e91ea405fdb331518951870f6b2e
Virginia <student@epicodus.com>
2015-03-02 11:25:37 -0800

    added semi-colon at line 66

--- car_dealership.php
+++ car_dealership.php
@@ -62,7 +62,7 @@
                                 echo "<li> $$car->price </li>";
                                 echo "<li> Miles: $car->miles </li>";
                                 echo "<li>Transmission: $car->transmission</li>";
-                                echo "<li>Motor: $car->motor</li>"
+                                echo "<li>Motor: $car->motor</li>";
                             echo "</ul>";
                         }
                     ?>
